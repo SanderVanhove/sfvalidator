b2adacd57e28e930cdf7431805fc96f759cc5622
Sander Vanhove <sander.vanhove@ugent.be>
2018-02-21 16:42:35 +0100

    Removed files.

--- index.js
+++
@@ -1,7 +0,0 @@
-var validator_class = require("./bin/validator.js");
-var validator = new validator_class();
-
-validator.validate_url("https://linked.open.gent/parking")
-	.then(result => {
-		console.log(result);
-	});
